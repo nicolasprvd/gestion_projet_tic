b1e22349c711da3b119ea7648e0970ade68a38d3
nicolasprvd <nicolas.preveraud@gmail.com>
2020-04-06 14:25:26 +0200

    User extra

--- src/main/java/com/app/projettic/web/rest/AccountResource.java
+++ src/main/java/com/app/projettic/web/rest/AccountResource.java
@@ -63,8 +63,7 @@ public class AccountResource {
         if (!checkPasswordLength(managedUserVM.getPassword())) {
             throw new InvalidPasswordException();
         }
-        User user = userService.registerUser(managedUserVM, managedUserVM.getPassword());
-        mailService.sendActivationEmail(user);
+        userService.registerUser(managedUserVM, managedUserVM.getPassword(), managedUserVM.getTypeUtilisateur());
     }
 
     /**
