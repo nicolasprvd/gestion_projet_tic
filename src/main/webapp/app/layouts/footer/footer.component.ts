71640c597422db78f5ab927e00cd6c8ba83c8421
Tiffany BUREAU <tiff.bureau@live.fr>
2020-04-09 20:38:32 +0200

    Rectification design

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'jhi-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['footer.scss']
+  templateUrl: './footer.component.html'
 })
 export class FooterComponent {}
