8d1099abf94df7c6e9001451c2ed6d4a414dcf04
Florent Kirch <florent.kirch@live.fr>
2020-04-10 11:44:06 +0200

    rating and sending to database? couldn't test it

--- src/main/webapp/app/entities/projet/project-rate.component.ts
+++ src/main/webapp/app/entities/projet/project-rate.component.ts
@@ -9,6 +9,11 @@ import { UserService } from 'app/core/user/user.service';
 import { UserExtraService } from 'app/entities/user-extra/user-extra.service';
 import { Account } from 'app/core/user/account.model';
 import { IUser } from 'app/core/user/user.model';
+import { IEvaluation } from 'app/shared/model/evaluation.model';
+import { SERVER_API_URL } from 'app/app.constants';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+
 // import { IDocument } from 'app/shared/model/document.model';
 
 @Component({
@@ -17,6 +22,8 @@ import { IUser } from 'app/core/user/user.model';
   styleUrls: ['./projet.scss']
 })
 export class ProjectRateComponent implements OnInit {
+  public resourceUrl = SERVER_API_URL + 'api/projets';
+
   project!: IProjet;
   account!: Account | null;
   authorities: string[] | undefined;
@@ -25,14 +32,19 @@ export class ProjectRateComponent implements OnInit {
   client!: IUser;
   typeUtilisateur?: TypeUtilisateur;
   login!: string | undefined;
-  finalRate: number;
+  specsRate = 0;
+  ganttsRate = 0;
+  outputRate = 0;
+  finalRate = (this.specsRate + this.ganttsRate + this.outputRate) / 3;
+  evaluation: IEvaluation;
 
   constructor(
     protected dataUtils: JhiDataUtils,
     protected activatedRoute: ActivatedRoute,
     private accountService: AccountService,
     private userService: UserService,
-    private userExtraService: UserExtraService
+    private userExtraService: UserExtraService,
+    private http: HttpClient
   ) {}
 
   ngOnInit(): void {
@@ -69,7 +81,21 @@ export class ProjectRateComponent implements OnInit {
     return this.typeUtilisateur === TypeUtilisateur.CLIENT;
   }
 
-  tmp() {
+  createEvaluation() {
+    let tmp = this;
     console.log(this.project.documents);
+    this.finalRate = (this.ganttsRate + this.outputRate + this.specsRate) / 3;
+    this.evaluation = new (class implements IEvaluation {
+      id: any;
+      noteCdc = tmp.specsRate;
+      noteFinale = tmp.finalRate;
+      noteRendu = tmp.outputRate;
+      noteSoutenance = tmp.ganttsRate;
+    })();
+    this.create(this.evaluation).subscribe();
+  }
+
+  create(evaluation: IEvaluation): Observable<IEvaluation> {
+    return this.http.post<IEvaluation>(this.resourceUrl, evaluation);
   }
 }
