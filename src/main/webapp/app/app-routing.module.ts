67e8b712750ffa423e40de06d0aea27d1b95d50e
Tiffany BUREAU <tiff.bureau@live.fr>
2020-04-17 17:20:56 +0200

    FAQ + Design

--- src/main/webapp/app/app-routing.module.ts
+++ src/main/webapp/app/app-routing.module.ts
@@ -6,8 +6,9 @@ import { DEBUG_INFO_ENABLED } from 'app/app.constants';
 import { Authority } from 'app/shared/constants/authority.constants';
 
 import { UserRouteAccessService } from 'app/core/auth/user-route-access-service';
+import { FAQ_ROUTE } from 'app/faq/faq.route';
 
-const LAYOUT_ROUTES = [navbarRoute, ...errorRoute];
+const LAYOUT_ROUTES = [navbarRoute, FAQ_ROUTE, ...errorRoute];
 
 @NgModule({
   imports: [
--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -6,6 +6,7 @@ import { ProjetticSharedModule } from 'app/shared/shared.module';
 import { ProjetticCoreModule } from 'app/core/core.module';
 import { ProjetticAppRoutingModule } from './app-routing.module';
 import { ProjetticHomeModule } from './home/home.module';
+import { ProjetticFAQModule } from './faq/faq.module';
 import { ProjetticEntityModule } from './entities/entity.module';
 // jhipster-needle-angular-add-module-import JHipster will add new module here
 import { MainComponent } from './layouts/main/main.component';
@@ -22,6 +23,7 @@ import { HeaderComponent } from 'app/layouts/header/header.component';
     ProjetticSharedModule,
     ProjetticCoreModule,
     ProjetticHomeModule,
+    ProjetticFAQModule,
     // jhipster-needle-angular-add-module JHipster will add new module here
     ProjetticEntityModule,
     ProjetticAppRoutingModule
