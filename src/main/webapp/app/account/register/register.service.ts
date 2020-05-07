c3163922ab1b978dab3b6572d6dbac00811eaf28
AudreyBalat <46654920+AudreyBalat@users.noreply.github.com>
2020-05-07 16:16:00 +0200

    merge dev audrey

--- src/main/webapp/app/account/register/register.service.ts
+++ src/main/webapp/app/account/register/register.service.ts
@@ -3,21 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { SERVER_API_URL } from 'app/app.constants';
+import { IUser } from 'app/core/user/user.model';
 
 @Injectable({ providedIn: 'root' })
 export class RegisterService {
   constructor(private http: HttpClient) {}
 
-  save(account: {
-    typeUtilisateur: any;
-    firstName: any;
-    lastName: any;
-    password: any;
-    activated: any;
-    langKey: string;
-    login: any;
-    email: any;
-  }): Observable<{}> {
+  save(account: IUser): Observable<{}> {
     return this.http.post(SERVER_API_URL + 'api/register', account);
   }
 }
